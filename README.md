b7526c288a8b2b3b00b663daa3477463a3f74f04
undx (Emmanuel GALLOIS) <emmanuel.gallois@agnesb.fr>
2012-10-19 11:03:38 +0200

    Updated for Redmine 2.1.x

---
+++ README.md
@@ -0,0 +1,16 @@
+# redirect_to_after_login - Redmine plugin
+
+This plugin redirects the user, just after he has logged, to the URL defined in settings.
+If the redirect url is not defined (default), the behaviour is as usual(My Page).
+
+## Installation
+
+    git clone https://github.com/undx/redmine_redirect_to_after_login.git redirect_to_after_login
+
+Please note that plugin *must be named* `redirect_to_after_login`.
+
+## Configuration
+
+Go to `Administration` > `Plugins` > `Configure` to set the URL.
+
+Enter the FQDN URL.
--- init.rb
+++ init.rb
@@ -4,11 +4,11 @@ Redmine::Plugin.register :redirect_to_after_login do
   name 'Redmine Redirect to url after login Plugin'
   author 'Emmanuel GALLOIS (undx)'
   description 'This plugin redirects the user, just after he has logged, to the URL defined in settings'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/undx/redmine_redirect_to_after_login'
   author_url 'https://github.com/undx/'
 
-  settings(:default => { 'redirect' => '' }, :partial => 'settings/redirectto_settings' )
+  settings(:default => { 'redirect' => '' }, :partial => 'settings/redirect_to_after_login_settings' )
 end
 
 module Hooks
